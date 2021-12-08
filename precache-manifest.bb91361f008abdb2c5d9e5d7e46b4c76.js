f99579c7a1474821415492d4243cd275ccf2ef78
LukeUkk <32644975+LukeUkk@users.noreply.github.com>
2021-12-08 23:11:05 +0000

    Updates

---
+++ precache-manifest.bb91361f008abdb2c5d9e5d7e46b4c76.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f31868fa61bd515b7bb1e0a4ae8bc74e",
+    "url": "/PortfolioWebstie/index.html"
+  },
+  {
+    "revision": "8f7c4d256c8746a54639",
+    "url": "/PortfolioWebstie/static/css/main.4bb3c007.chunk.css"
+  },
+  {
+    "revision": "7a90da2778865b2821db",
+    "url": "/PortfolioWebstie/static/js/2.145c7379.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/PortfolioWebstie/static/js/2.145c7379.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f7c4d256c8746a54639",
+    "url": "/PortfolioWebstie/static/js/main.f821444f.chunk.js"
+  },
+  {
+    "revision": "28f7a55f7463b5f5da24",
+    "url": "/PortfolioWebstie/static/js/runtime-main.0f6baa12.js"
+  },
+  {
+    "revision": "7c8e5f50725885d51b72b45164274e41",
+    "url": "/PortfolioWebstie/static/media/avatar.7c8e5f50.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/PortfolioWebstie/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/PortfolioWebstie/static/media/html-css-javascript-lg.54fd4234.jpg"
+  },
+  {
+    "revision": "2c7701aae07adecdb0a7e336dff08def",
+    "url": "/PortfolioWebstie/static/media/html-css-javascript.2c7701aa.jpg"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/PortfolioWebstie/static/media/javascript-fullstack.730958a2.jpg"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "/PortfolioWebstie/static/media/mern-stack.90616a5d.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/PortfolioWebstie/static/media/react-redux.a8b012a0.jpg"
+  },
+  {
+    "revision": "f370c1e0adb80194dd3ea10e468bc69a",
+    "url": "/PortfolioWebstie/static/media/react.f370c1e0.png"
+  }
+]);
